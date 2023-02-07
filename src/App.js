4fcbb39167bf413000f1ab03c42162c7db34218b
VincentShao32 <yunbo2016@gmail.com>
2023-02-06 23:31:23 -0800

    new tailwind config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="bg-slate-400">
-      <h1 className="text-6xl px-4">SpartaSite!</h1>
+      <h1 className="text-6xl px-[600px]">SpartaSite!</h1>
     </div>
   );
 }
