575e6b6210836bb6ee25f93f5f501cadf771bb06
Elliot HH <eharhar06@proton.me>
2023-05-18 18:39:49 -0700

    made navbar tab words pink
    like 5 letters or smth

--- client/src/components/ElliotIggy_NavBarTab.js
+++ client/src/components/ElliotIggy_NavBarTab.js
@@ -5,7 +5,7 @@ const NavbarTab = ({ path, text, onClick }) => {
   return (
     <Link
       to={path}
-      className={` transition-all delay-150 bg-size-200 bg-pos-0 font-productsans font-bold text-sm px-5 mr-2 mb-2 flex-initial block text-center text-md rounded text-white/75 w-[100px] py-4 
+      className={` transition-all delay-150 bg-size-200 bg-pos-0 font-productsans font-bold text-sm px-5 mr-2 mb-2 flex-initial block text-center text-md rounded text-frcpink/75 w-[100px] py-4 
       hover:bg-pos-100 duration-500 hover:text-white/80 hover:scale-110 hover:bg-gradient-to-r from-frcpink/40 via-frcyellow/40 to-frcpurple/40`}
       onClick={onClick}
     >
