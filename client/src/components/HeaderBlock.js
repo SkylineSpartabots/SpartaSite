4c759395332d8f83c458402d9ca0401c9706c790
Gam3rr <84348006+Gam3rrXD@users.noreply.github.com>
2024-01-03 23:09:34 -0800

    Update HeaderBlock.js
    Color change to keep upto date

--- client/src/components/HeaderBlock.js
+++ client/src/components/HeaderBlock.js
@@ -1,7 +1,7 @@
 const HeaderBlock = ({ text }) => {
   return (
     <div className="max-w-[1000px] w-full text-center bg-gray-700 rounded p-4">
-      <h1 className="mx-auto w-fit px-2 font-scoutcond font-black uppercase text-6xl italic text-transparent bg-clip-text bg-gradient-to-r from-white via-frcyellow to-frcpurple">
+      <h1 className="mx-auto w-fit px-2 font-scoutcond font-black uppercase text-6xl italic text-transparent bg-clip-text bg-gradient-to-r from-frcpurple via-frcpink to-frclilac">
         {text}
       </h1>
     </div>
