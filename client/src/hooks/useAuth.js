52332da6765b0b6efe8259ad10f901bf9c3bd26f
Gam3rrXD <EshaanE@proton.me>
2024-01-09 22:36:51 -0800

    Revert "Removing old auth"
    This reverts commit f64487aca8c01455d22c5c082b600797549072d7.

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
+
+export function useAuth(type) {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const navigate = useNavigate();
+
+  async function auth(user, password) {
+    setIsLoading(true);
+    setError(null);
+    const ute = process.env.VAL;
+    const fet = process.env.USE;
+
+    const enc = btoa(user + ":" + password);
+
+    if (enc === fet) {
+      localStorage.setItem("user", ute);
+      setIsLoading(false);
+      navigate("/");
+    } else {
+      setIsLoading(false);
+      setError("Invalid credentials");
+    }
+  }
+
+  return { auth, isLoading, error };
+}
+
+export default useAuth;
