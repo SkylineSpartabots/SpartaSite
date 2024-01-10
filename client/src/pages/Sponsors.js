d13eb1d54b8d793206cbcf978ecc5d7b7356ea86
Gam3rrXD <EshaanE@proton.me>
2024-01-10 13:57:21 -0800

    White sponsor header

--- client/src/pages/Sponsors.js
+++ client/src/pages/Sponsors.js
@@ -44,10 +44,10 @@ const Sponsors = () => {
 
   return (
     <div className="pt-[135px] pb-8 bg-slate-100 flex flex-col items-center gap-8">
-      <HeaderBlock text="Sponsors" />
+      <HeaderBlock text={"Sponsors"}/>
       <div className="box flex flex-col items-center">
         <h1 className="flex-initial font-scountcond font-black text-5xl mx-auto w-fit pb-4 italic">
-          2023
+          2024
         </h1>
         <div className=" flex-initial flex flex-row flex-wrap gap-8 px-12 justify-center">
           {/* loop through all the logos, creating an image tag for each */}
