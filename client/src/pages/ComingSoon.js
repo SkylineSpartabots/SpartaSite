f216d7dc3357a213e43fe3d35495e365e4ebff11
Ryan Lin <solar138@outlook.com>
2024-01-14 13:51:36 -0800

    removed white bg around smiley icon on coming soon page

--- client/src/pages/ComingSoon.js
+++ client/src/pages/ComingSoon.js
@@ -1,10 +1,11 @@
 import React from "react";
+import thumbsUpSmiley from "../graphics/images/thumbs-up-smiley-36.png";
 
 const ComingSoon = () => {
   return (
     <div className="h-screen pt-[135px] px-[75px] bg-gray-100 flex flex-col items-center justify-center">
       <img
-        src="https://clipartmag.com/images/thumbs-up-smiley-36.jpg"
+        src={thumbsUpSmiley}
         alt="Under Construction"
         className="mb-8"
         style={{ width: "500px", height: "500px" }}
