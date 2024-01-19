08eafa244545f2ccc9914590fbe6636c940ac517
Ryan Lin <solar138@outlook.com>
2024-01-18 18:43:42 -0800

    added black gradient to navbar for more visibility on light backgrounds

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -18,14 +18,14 @@ const NavbarItem = ({ path, name, loc }) => {
 const Navbar = () => {
   let location = useLocation();
   return (
-    <nav className="flex fixed top-0 w-full bg-transparent z-10">
+    <nav className="flex fixed top-0 w-full black-fade-top z-10">
       <a
         href="/"
         className="text-2xl md:text-3xl font-scoutcond italic font-extrabold whitespace-nowrap text-white ml-6 mt-3"
       >
         THE SPARTABOTS
       </a>
-      <div className="flex top-0 w-full gap-4 justify-end ">
+      <div className="flex top-0 w-full gap-4 justify-end">
         <NavbarItem path="/" name="Home" loc={location} />
         <NavbarItem path="/ourteam" name="Our Team" loc={location} />
         <NavbarItem path="/history" name="History" loc={location} />
