699beb181a08cb43ccf726a6775f386d08d879a8
Ryan Lin <solar138@outlook.com>
2024-01-19 23:08:14 -0800

    switched order of name and date on history page so the dates are aligned

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -18,19 +18,19 @@ const OurTeam = () => {
       />
       <div className="bg-slate-100 flex flex-col gap-8 items-center p-8">
         <Card
-          title="Chronos: 2022"
+          title="2022 — Chronos"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           image={ChronosPhoto}
           alt="2022 FRC robot named chronos"
         />
         <Card
-          title="Zeus: 2023"
+          title="2023 — Zeus"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           image={ChronosPhoto}
           alt="2023 FRC robot named zeus"
         />
         <Card
-          title="Artemis: 2024"
+          title="2024 — Artemis"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           image={ChronosPhoto}
           alt="2024 FRC robot named artemis"
