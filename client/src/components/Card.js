fb6c6a2118dc312406c1dbc39bb6bb897c840421
Ryan Lin <solar138@outlook.com>
2024-05-09 12:10:09 -0700

    Fixed LinkConverter.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,9 +4,9 @@ import LinkConverter from "./LinkConverter";
 const Card = ({ title, text, image, alt, allowLinks = false }) => {
   
   var textElement = (<p className="font-productsans text-base text-justify">{text}</p>)
-  if (allowLinks) {
-    textElement = LinkConverter(textElement);
-  }
+
+  
+  textElement = LinkConverter(textElement);
   var card = (
     <div className="flex flex-col sm:flex-row flex-1 px-6 py-4 max-w-[1000px] bg-white rounded gap-4 min-h-[300px]">
       <div className="flex-1 max-w-[500px] break-words">
