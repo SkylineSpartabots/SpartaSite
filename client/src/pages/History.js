d6028921baf54e614e8bad96eb801493dae6c371
Ryan Lin <solar138@outlook.com>
2024-05-10 08:43:14 -0700

    Update more things to use new new lines.

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -17,13 +17,13 @@ const OurTeam = () => {
       <div className="bg-slate-100 flex flex-col gap-8 items-center p-8">
         <Card
           title="2022 — Chronos"
-          text="Chronos was the Spartabots' 2022 competition robot for the FIRST challenge RAPID REACT, this robot made it to World's in the Hopper divison as a Semi-Finalist. Status: Decommissioned"
+          text={["Chronos was the Spartabots' 2022 competition robot for the FIRST challenge RAPID REACT, this robot made it to World's in the Hopper divison as a Semi-Finalist.", "Status: Decommissioned"]}
           image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/Chronos"}
           alt="2022 FRC robot named chronos"
         />
         <Card
           title="2023 — Zeus"
-          text="Zeus was the Spartabots' 2023 robot for the FIRST Challenge CHARGED UP, this robot was apart of Alliance 5 at Sammamish and Alliance 6 at Glacier Peak! Status: Decommissioned"
+          text={["Zeus was the Spartabots' 2023 robot for the FIRST Challenge CHARGED UP, this robot was apart of Alliance 5 at Sammamish and Alliance 6 at Glacier Peak!", "Status: Decommissioned"]}
           image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/Zeus"}
           alt="2023 FRC robot named zeus"
         />
