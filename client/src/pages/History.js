ee9a564c2a5ac4c86e8793912cc57a9c06b7908e
Gam3rr <EshaanE@proton.me>
2024-05-12 00:16:08 -0700

    spelling fix

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -76,19 +76,19 @@ const OurTeam = () => {
             alt="2020 FRC Compition Robot OLYMPUS"
         />
         <Card
-          title="2022 — Chronos"
+          title="2022 — CHRONOS"
           text={["Chronos was the Spartabots' 2022 competition robot for the FIRST challenge RAPID REACT, this robot made it to World's in the Hopper divison as a Semi-Finalist.", "Robot Status: Decommissioned"]}
           image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/Chronos"}
           alt="2022 FRC robot named chronos"
         />
         <Card
-          title="2023 — Zeus"
+          title="2023 — ZEUS"
           text={["Zeus was the Spartabots' 2023 robot for the FIRST Challenge CHARGED UP, this robot was apart of Alliance 5 at Sammamish and Alliance 6 at Glacier Peak!", "Robot Status: Decommissioned"]}
           image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/Zeus"}
           alt="2023 FRC robot named zeus"
         />
         <Card
-          title="2024 — Artemis"
+          title="2024 — ARTEMIS"
           text={["Artemis is the Spartabots' current robot for the FIRST Challenge CRESCENDO, this robot was an Event Finalist at Auburn and a Semi-Finalist at Sammamish!", "Robot Status: In Use"]}
           image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/Artemis"}
           alt="2024 FRC robot named artemis"
