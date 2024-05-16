add34f2b1fb15cfe73823cbe2acefa37c134322d
Ryan Lin <solar138@outlook.com>
2024-05-16 13:56:42 -0700

    Fixed empty space at the right of the footer.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -81,7 +81,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="mx-6 py-10 text-center md:text-left">
-        <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           <div className="">
             <h1
               className="
