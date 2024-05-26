3be65147cb3e12caf3455183663f0cd1242170b9
eggliot <107228567+messier81porcupine@users.noreply.github.com>
2024-05-26 00:11:27 -0700

    added dark mode to 404 page - goat photo??

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return (
-    <div className="h-screen pt-[135px] px-[75px] bg-gray-100">
-      <h1 className="font-scoutcond font-black uppercase text-6xl py-4">
+  return ( //  TODO Add a pic of the goat for the lost screen
+    <div className="h-screen pt-[135px] px-[75px] bgColor">
+      <h1 className="font-scoutcond font-black uppercase text-6xl py-4 textColor">
         Looks like you're lost!
       </h1>
-      <p className="font-productsans text-xl">
+      <p className="font-productsans text-xl textColor">
         Use the navbar at the top to <a href="/">get back on track.</a>
       </p>
     </div>
