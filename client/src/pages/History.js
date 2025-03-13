fa6c6950ea0f2a20db48900ccb7632bf0063c6dc
AidanChen0923 <aidan.chen@outlook.com>
2025-03-13 15:35:50 -0700

    Added a few Images
    Added images to history

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -7,6 +7,8 @@ import ZeusImg from "../graphics/images/Zeus.png"
 import ChronosImg from "../graphics/images/Chronos.webp"
 import Olympus from "../graphics/images/OLD/Olympus.JPG"
 import Khaos from "../graphics/images/OLD/Khaos.jpg"
+import Hermes from "../graphics/images/OLD/Hermes.png"
+import First from "../graphics/images/FIRST_logo.png"
 
 const History = () => {
     const [themebool, setthemebool] = useState(true)
@@ -59,49 +61,49 @@ const History = () => {
                 <Card
                     title="[history/2018](2018 — HERMES)"
                     text={["Hermes was the Spartabots' 2018 robot for the FIRST Challenge POWER UP, we were the WORLD CHAMPIONS with a qualifying record of 41-30-0. This was our first year with robot names!"]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2018%20Bot%20(HERMES)%20~%20FIRST%20POWER%20UP"}
+                    image={Hermes}
                     alt="2018 FRC Competition Robot HERMES"
                 />
                 <Card
                     title="[history/2017](2017 — FIRST STEAMWORKS)"
                     text={["The Spartabots participated in the FIRST STEAMWORKS challenge at Auburn Mountainview and Glacier Peak, our qualifying record was 12-12-2."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2017%20Bot%20~%20FIRST%20STEAMWORKS"}
+                    image={First}
                     alt="2017 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2016](2016 — FIRST STONGHOLD)"
                     text={["In the FIRST STRONGHOLD Challenge, The Spartabots were winners of the Glacier Peak event with a qualifying record of 12-13-0."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2016%20Bot%20~%20FIRST%20STRONGHOLD"}
+                    image={First}
                     alt="2016 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2015](2015 — FIRST RECYCLE RUSH)"
                     text={["Spartabots participated in the FIRST RECYCLE RUSH challenge at Auburn Mountainview and Mount Vernon Event, our qualifying score was 57.83, our playoff score was 63.25."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2015%20Bot%20~%20FIRST%20RECYCLE%20RUSH"}
+                    image={First}
                     alt="2015 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2014](2014 — FIRST ARIEL ASSIST)"
                     text={["In the 2014 competition season, The Spartabots were FINALISTS at Glacier Peak and were DCMP participants, with a qualifying record of 22-21-1."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2014%20Bot%20~%20FIRST%20ARIEL%20ASSIST"}
+                    image={First}
                     alt="2014 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2013](2013 — FIRST ULTIMATE ASCENT)"
                     text={["The Spartabots participated in the FIRST ULTIMATE ASCENT challenge at the Central Washington Regional, we won the Creativity Award sponsored by Xerox and our qualifying record was 7-4-0."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2013%20Bot%20~%20FIRST%20ULTIMATE%20ASCENT"}
+                    image={First}
                     alt="2013 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2012](2012 — FIRST REBOUND RUMBLE)"
                     text={["The Spartabots participated in the FIRST REBOUND RUMBLE challenge at the Seattle Cascade Regional, our qualifying record was 3-8-0."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2012%20Bot%20~%20FIRST%20REBOUND%20RUMBLE"}
+                    image={First}
                     alt="2012 FRC Competition Robot"
                 />
                 <Card
                     title="[history/2011](2011 — FIRST LOGO MOTION)"
                     text={["The Spartabots participated in the FIRST LOGO MOTION challenge at the Seattle Olympic Regional, our qualifying record was 4-4-2."]}
-                    image={"https://res.cloudinary.com/spartasite/image/upload/f_auto,q_auto/v1/RobotImages/2011%20Bot%20~%20FIRST%20LOGO%20MOTION"}
+                    image={First}
                     alt="2011 FRC Competition Robot"
                 />
             </div>
